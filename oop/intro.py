feb99682e14b73f006c299168e4670d7e26c5bdf
Utsav <utsavchatterjee71@gmail.com>
2023-06-26 19:31:58 +0530

    update into.py

--- oop/intro.py
+++ oop/intro.py
@@ -9,11 +9,13 @@ class Person1:
         return self.name
     
 class Person2:
+    msg="hi"
 
     def __init__(self, name:str):
         self.name=name
 
     def getName(self)->str:
+        print(self.msg)
         return self.name
 
 if __name__=="__main__":
@@ -26,5 +28,5 @@ if __name__=="__main__":
     #another type of declaring......
     p1=Person2("utsav")
     p2=Person2("supu")
-    print(Person1.getName(p1)) #----> self is pointing to p1 now
-    print(Person1.getName(p2)) #----> self is pointing to p2 now
+    print(Person2.getName(p1)) #----> self is pointing to p1 now
+    print(Person2.getName(p2)) #----> self is pointing to p2 now
