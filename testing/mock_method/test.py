9c121ebaee61d4cd6f11bba1bb0c029ef76c84ce
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-07-09 13:02:26 +0530

    update mocke test

--- testing/mock_method/test.py
+++ testing/mock_method/test.py
@@ -3,31 +3,31 @@ import unittest
 from unittest import TestCase
 from unittest.mock import Mock,patch
 
-BASE_URL='https://todo.pixegami.io'
+"""
+https://docs.python.org/3/library/unittest.mock-examples.html#mock-patching-methods
+"""
 
-def create_task():
-    url=BASE_URL+'/create-task'
-    data={
-        "content": "hey",
-        "user_id": "134",
-        "task_id": "t134",
-        "is_done": False
-    }
-    res=requests.put(url=url,json=data)
-    return res.status_code
+BASE_URL='https://dummyjson.com/products'
+
+def get_data(ids):
+    url=BASE_URL+f'/{ids}'
+    res=requests.get(url=url)
+    return res.json()
 
 class Test(TestCase):
 
-    @patch('requests.put')
-    def test_create_task(self,mock_get):
+    @patch('requests.get')
+    def test_get_data(self,mock_get):
         mock_res=Mock()
-        expected_res_status=200
-        mock_res.json.status_code.return_value=expected_res_status
+        expected_res={"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]}
+
+        mock_res.json.return_value=expected_res
 
         mock_get.return_value=mock_res
 
-        createTask=create_task()
-        self.assertEqual(createTask,expected_res_status)
+        getData=get_data(1)
+
+        self.assertEqual(getData,expected_res)
 
 if __name__=='__main__':
     unittest.main()
